eaeee5f8e27aa139109fc9ae3e017f78c5f8db43
Petr Skoda <info@skodak.org>
2010-08-01 20:05:20 +0200

    updated to new settings style

--- settings.php
+++ settings.php
@@ -26,20 +26,22 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-//--- modedit defaults -----------------------------------------------------------------------------------
-$settings->add(new admin_setting_heading('consultationmodeditdefaults', get_string('modeditdefaults', 'consultation'), get_string('modeditdefaultsexplain', 'consultation')));
+if ($ADMIN->fulltree) {
+    //--- modedit defaults -----------------------------------------------------------------------------------
+    $settings->add(new admin_setting_heading('consultationmodeditdefaults', get_string('modeditdefaults', 'consultation'), get_string('modeditdefaultsexplain', 'consultation')));
 
-$advstr = get_string('advanced').' ';
+    $advstr = get_string('advanced').' ';
 
-$options = array (0=>get_string('no'), 1=>1, 5=>5, 10=>10, 30=>30, 60=>60);
-$settings->add(new admin_setting_configselect_with_advanced('consultation/edittime', get_string('modedittime', 'consultation'), get_string('modedittimeexplain', 'consultation'), array('value'=>0, 'adv'=>false), $options));
+    $options = array (0=>get_string('no'), 1=>1, 5=>5, 10=>10, 30=>30, 60=>60);
+    $settings->add(new admin_setting_configselect_with_advanced('consultation/edittime', get_string('modedittime', 'consultation'), get_string('modedittimeexplain', 'consultation'), array('value'=>0, 'adv'=>false), $options));
 
-$options = array (0=>get_string('no'), 1=>1, 2=>2, 3=>3, 4=>4, 5=>5, 6=>6, 7=>7, 8=>8, 9=>9, 10=>10, 25=>25, 50=>50, 100=>100);
-$settings->add(new admin_setting_configselect_with_advanced('consultation/openlimit', get_string('modopenlimit', 'consultation'), get_string('modopenlimitexplain', 'consultation'), array('value'=>0, 'adv'=>false), $options));
+    $options = array (0=>get_string('no'), 1=>1, 2=>2, 3=>3, 4=>4, 5=>5, 6=>6, 7=>7, 8=>8, 9=>9, 10=>10, 25=>25, 50=>50, 100=>100);
+    $settings->add(new admin_setting_configselect_with_advanced('consultation/openlimit', get_string('modopenlimit', 'consultation'), get_string('modopenlimitexplain', 'consultation'), array('value'=>0, 'adv'=>false), $options));
 
-$strdays = ' '.get_string('days');
-$options = array (0=>get_string('never'), 7=>'7'.$strdays, 14=>'14'.$strdays, 30=>'30'.$strdays, 150=>'150'.$strdays, 365=>'365'.$strdays);
-$settings->add(new admin_setting_configselect_with_advanced('consultation/deleteafter', get_string('moddeleteafter', 'consultation'), get_string('moddeleteafterexplain', 'consultation'), array('value'=>0, 'adv'=>false), $options));
+    $strdays = ' '.get_string('days');
+    $options = array (0=>get_string('never'), 7=>'7'.$strdays, 14=>'14'.$strdays, 30=>'30'.$strdays, 150=>'150'.$strdays, 365=>'365'.$strdays);
+    $settings->add(new admin_setting_configselect_with_advanced('consultation/deleteafter', get_string('moddeleteafter', 'consultation'), get_string('moddeleteafterexplain', 'consultation'), array('value'=>0, 'adv'=>false), $options));
 
-$options = array(0=>get_string('no'), 1=>get_string('yes'));
-$settings->add(new admin_setting_configselect_with_advanced('consultation/notify', get_string('modnotify', 'consultation'), get_string('modnotifyexplain', 'consultation'), array('value'=>0, 'adv'=>false), $options));
+    $options = array(0=>get_string('no'), 1=>get_string('yes'));
+    $settings->add(new admin_setting_configselect_with_advanced('consultation/notify', get_string('modnotify', 'consultation'), get_string('modnotifyexplain', 'consultation'), array('value'=>0, 'adv'=>false), $options));
+}
